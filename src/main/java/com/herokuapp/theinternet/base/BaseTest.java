8368041822959208a0ea27c78778846d1eb15e82
AmandaRiosAmaral <amandariosamaral@gmail.com>
2022-12-30 14:07:52 +0100

    add Test Listeners

--- src/main/java/com/herokuapp/theinternet/base/BaseTest.java
+++ src/main/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -1,16 +1,18 @@
 package com.herokuapp.theinternet.base;
 
 import java.lang.reflect.Method;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+
+@Listeners({ com.herokuapp.theinternet.base.TestListener.class })
 public class BaseTest {
 
 	protected WebDriver driver;
